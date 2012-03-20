0a9207bbe863219b449fec5ad706461b85cae9b6
Tala Huhe <thuhe@cs.brown.edu>
2012-03-20 00:04:37 -0400

    Testing index

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    // response.send('Hello World!');
+    response.render("index.html");
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
