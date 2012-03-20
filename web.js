95386155722fd4ef36cf9f6b0f8b7b8dd84c2137
Tala Huhe <thuhe@cs.brown.edu>
2012-03-20 00:07:02 -0400

    Oops.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     // response.send('Hello World!');
-    response.render("index.html");
+    response.sendfile("index.html");
 });
 
 var port = process.env.PORT || 3000;
